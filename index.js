3c6a85f681674aa0483fa56bdc60020dae9e9150
jbeardles <jbeardles.ynwa@gmail.com>
2020-12-15 08:05:19 -0600

    Add dotenv package.:

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
+
 const discord = require('discord.js');
 const client = new discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+})
 
 client.on('message', msg => {
     if (msg.content === "!vizbot" || msg.content === "!kazbot") {
