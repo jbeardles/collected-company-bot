8047db4aba29c631373f4c851212a6404a5021cb
jbeardles <jbeardles.ynwa@gmail.com>
2020-12-15 08:48:40 -0600

    Set up pg and add prefix.

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ require('dotenv').config();
 
 const discord = require('discord.js');
 const client = new discord.Client();
+const db = require('./src/db');
+
+db.connect(err => {
+    if (err) {
+        console.error('connection error', err.stack);
+    } else {
+        console.log('connected to pg instance');
+    }
+});
 
 client.on('ready', () => {
     console.log('I am ready!');
 })
 
 client.on('message', msg => {
-    console.log(`Message received: ${msg}`)
-    if (msg.content === "!vizbot" || msg.content === "!kazbot") {
+    const { content, channel } = msg;
+    if (content.slice(0, 2) !== '!cc' || channel !== 'bot-text') {
+        return;
+    }
+
+    if (msg.content === "!cc vizbot" || msg.content === "!cc kazbot") {
         msg.channel.send("Hellllllll Yeahhhhhh");
     }
 })
